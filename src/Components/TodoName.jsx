69e4a101e28481229cd42fbbfc91425bc95225e9
basharatali112 <basharat62200150@gmail.com>
2025-05-10 14:19:04 +0500

    Name Change + tabName change

--- src/Components/TodoName.jsx
+++ src/Components/TodoName.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const TodoName = () => {
   return (
-    <div className='name'>TODO App</div>
-  )
-}
+    <div className="text-center my-4">
+      <h1 className="display-5 fw-bold text-success">TODO App</h1>
+    </div>
+  );
+};
 
-export default TodoName
+export default TodoName;
